bca813e64913e0c5563afa260fa73a3bdabf227a
Antonio Nuñez <annunez1291@gmail.com>
2022-05-13 23:37:04 -0400

    casi completo 90%
    Busca pokemon o habilidades pero no los dos. Hay que arreglar el utils para getabilities

--- parcial2/index.js
+++ parcial2/index.js
@@ -1,81 +1,89 @@
 ((Utils) => {
-    const App = {
-      htmlElements: {
-        pokemonFinderForm: document.querySelector("#pokemon-finder-form"),
-        pokemonFinderSearchType: document.querySelector(
-          "#pokemon-finder-search-type"
-        ),
-        pokemonFinderInput: document.querySelector("#pokemon-finder-query"),
-        pokemonFinderOutput: document.querySelector("#pokemon-finder-response"),
-      },
-      init: () => {
-        App.htmlElements.pokemonFinderForm.addEventListener(
-          "submit",
-          App.handlers.pokemonFinderFormOnSubmit
-        );
-      },
-      handlers: {
-        pokemonFinderFormOnSubmit: async (e) => {
-          e.preventDefault();
-  
-          const query = App.htmlElements.pokemonFinderInput.value;
-          const searchType = App.htmlElements.pokemonFinderSearchType.value;
-          console.log({ searchType });
-          try {
-            const response = await Utils.getPokemon({
-              query,
-              searchType,
-            });
-            const renderedTemplate = App.templates.render({
-              searchType,
-              response,
-            });
-            App.htmlElements.pokemonFinderOutput.innerHTML = renderedTemplate;
-          } catch (error) {
-            App.htmlElements.pokemonFinderOutput.innerHTML = `<h1>${error}</h1>`;
-          }
-        },
+  const App = {
+    htmlElements: {
+      pokemonFinderForm: document.querySelector("#pokemon-finder-form"),
+      pokemonFinderSearchType: document.querySelector(
+        "#pokemon-finder-search-type"
+      ),
+      pokemonFinderInput: document.querySelector("#pokemon-finder-query"),
+      pokemonFinderOutput: document.querySelector("#pokemon-finder-response"),
+    },
+    init: () => {
+      App.htmlElements.pokemonFinderForm.addEventListener(
+        "submit",
+        App.handlers.pokemonFinderFormOnSubmit
+      );
+    },
+    handlers: {
+      pokemonFinderFormOnSubmit: async (e) => {
+        e.preventDefault();
+
+        const query = App.htmlElements.pokemonFinderInput.value;
+        const searchType = App.htmlElements.pokemonFinderSearchType.value;
+        console.log({ searchType });
+        try {
+          const response = await Utils.getPokemon({
+            query,
+            searchType,
+          });
+          const renderedTemplate = App.templates.render({
+            searchType,
+            response,
+          });
+          App.htmlElements.pokemonFinderOutput.innerHTML = renderedTemplate;
+        } catch (error) {
+          App.htmlElements.pokemonFinderOutput.innerHTML = `<h1>${error}</h1>`;
+        }
       },
       
-      templates: {
-        render: ({ searchType, response }) => {
-          const renderMap = {
-            ability: App.templates.abilityCard,
-            pokemon: App.templates.pokemonCard,
-          };
-          return renderMap[searchType]
-            ? renderMap[searchType](response)
-            : App.templates.errorCard();
-        },
-        errorCard: () => `<h1>There was an error</h1>`,
 
-
-
-        pokemonCard: ({ id, sprites, types, name, weight, height, abilities, species }) => {
-          
-          return `<div class="poke-card"><h1>${name} (${id})</h1>
-          <div class="img-container">
-          <img class="poke-img" src="${sprites.front_default}"></img>
-          <img class="poke-img-back" src="${sprites.back_default}"></img>
-          </div>
-          <div class="poke-types">Types: ${types[0].type.name}   ${types[1]? types[1].type.name : ''} </div>
-          <li>Weight: ${weight/10} kgs</li>
-          <li>Height: ${height/10} mts</li>
-          <li>Abilities: ${abilities[0].ability.name}, ${abilities[1]? abilities[1].ability.name : ''}</li>
-          </div></div> 
-          <button class="btn-clear" onclick="history.go(0);">Clear</button>`;
-        },
-
-        abilityCard: ({ id, name, pokemon }) => {
-          const pokemonList = pokemon.map(
-            ({ pokemon, is_hidden }) =>
-              `<div class="poke-card"><li>${pokemon.name}${
-                is_hidden ? " (Hidden)" : ""
-              }</li></div>`
-          );
-          return `<h1><div class="poke-types">${name} (${id})</div></h1><ul>${pokemonList.join("")}</ul>`;
-        },
+    },
+    
+    templates: {
+      render: ({ searchType, response }) => {
+        const renderMap = {
+          ability: App.templates.abilityCard,
+          pokemon: App.templates.pokemonCard,
+        };
+        return renderMap[searchType]
+          ? renderMap[searchType](response)
+          : App.templates.errorCard();
       },
-    };
-    App.init();
-  })(document.Utils);
+      errorCard: () => `<h1>There was an error</h1>`,
+
+    
+      pokemonCard: ({ id, sprites, types, name, weight, height, abilities, species, evolutionChain}) => {
+        const evolutions = Utils.getEvolutionsFromEvolutionChain(evolutionChain);
+        return `<div class="poke-card-display">
+        <div class= "poke-container-left">
+        <h1>${name} (${id})</h1>
+        <div class="img-container">
+        <img class="poke-img" src="${sprites.front_default}"></img>
+        <img class="poke-img-back" src="${sprites.back_default}"></img></div>
+
+        <div class="poke-types">Types: ${types[0].type.name} ${types[1]? types[1].type.name : ''}</div>
+        <div class="poke-types">Evolution Chain:${evolutions}</div>
+        <div class="poke-container-right">
+        <li>Weight: ${weight/10} kgs</li>
+        <li>Height: ${height/10} mts</li>
+        <li>Abilities: <label>${abilities[0].ability.name},</label> <label>${abilities[1]? abilities[1].ability.name : ''}</label></li></div>
+        </div>
+        
+        <button class="btn-clear" onclick="history.go(0);">Clear</button> 
+        </div>`;
+    
+      },
+
+      abilityCard: ({ id, name, pokemon }) => {
+        const pokemonList = pokemon.map(
+          ({ pokemon, is_hidden }) =>
+            `<div class="poke-card"><li>${pokemon.name}${
+              is_hidden ? " (Hidden)" : ""
+            }</li></div>`
+        );
+        return `<h1><div class="poke-types">${name} (${id})</div></h1><ul>${pokemonList.join("")}</ul>`;
+      },
+    },
+  };
+  App.init();
+})(document.Utils);
--- parcial2/utils.js
+++ parcial2/utils.js
@@ -1,28 +1,58 @@
 (() => {
-    const Utils = {
-      settings: {
-        backendBaseUrl: "https://pokeapi.co/api/v2",
-      },
-      getFormattedBackendUrl: ({ query, searchType }) => {
-        return `${Utils.settings.backendBaseUrl}/${searchType}/${query}`;
-      },
-      getPokemon: ({ query, searchType = "pokemon" }) => {
-        return Utils.fetch({
-          url: Utils.getFormattedBackendUrl({ query, searchType }),
-          searchType,
-        });
-      },
-      fetch: async ({ url, searchType }) => {
-        try {
-          const rawResponse = await fetch(url);
-          if (rawResponse.status !== 200) {
-            throw new Error(`${searchType} not found`);
-          }
-          return rawResponse.json();
-        } catch (error) {
-          throw error;
+  const Utils = {
+    settings: {
+      backendBaseUrl: "https://pokeapi.co/api/v2",
+    },
+    getFormattedBackendUrl: ({ query, searchType }) => {
+      return `${Utils.settings.backendBaseUrl}/${searchType}/${query}`;
+    },
+    getPokemon: async ({ query, searchType = "pokemon" }) => {
+      const pokemon = await Utils.fetch({
+        url: Utils.getFormattedBackendUrl({ query, searchType }),
+        searchType,
+      });
+      const species = await Utils.getSpecies(pokemon.species.url);
+      const evolutionChain = await Utils.getEvolutionChain({
+        url: species.evolution_chain.url,
+        query: pokemon.order,
+      });
+      pokemon.evolutionChain = evolutionChain;
+      return pokemon;
+    },
+    getSpecies: async (url, searchType = "pokemon-species") => {
+      return await Utils.fetch({ url, searchType });
+    },
+    getEvolutionChain: async ({ url, searchType = "evolution-chain" }) => {
+      return await Utils.fetch({
+        url,
+        searchType,
+      });
+    },
+    getEvolutionsFromEvolutionChain: (evolutionChain) => {
+      const evolutions = [];
+      const getEvolutionsRecursive = (evolutionNext) => {
+        evolutions.push(evolutionNext.species.name);
+        if (evolutionNext.evolves_to[0]) {
+          getEvolutionsRecursive(evolutionNext.evolves_to[0]);
         }
-      },
-    };
-    document.Utils = Utils;
-  })();
+      };
+      if (evolutionChain.chain) {
+        getEvolutionsRecursive(evolutionChain.chain);
+      }
+
+      return evolutions;
+    },
+    fetch: async ({ url, searchType }) => {
+      try {
+        const rawResponse = await fetch(url);
+        if (rawResponse.status !== 200) {
+          throw new Error(`${searchType} not found`);
+        }
+        return rawResponse.json();
+      } catch (error) {
+        throw error;
+      }
+    },
+  };
+  document.Utils = Utils;
+})();
